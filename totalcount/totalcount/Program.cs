8376a12db5261f5712c8a4fb850997d669b5369a
kuzhali <kuzhalis@gmail.com>
2016-02-05 21:46:12 -0600

    This is totalcount.

---
+++ totalcount/totalcount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totalcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputnum, i;
+            int n;
+            Console.WriteLine("Enter the totalcount : ");
+            n = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("enter the operation :");
+             string oper = Console.ReadLine();
+            if (oper == "add")
+            {
+                int r = 0;
+                for (i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Enter the number " + (i + 1));
+                    inputnum = Int32.Parse(Console.ReadLine());
+                    
+                    r = r + inputnum;
+
+                }
+                Console.WriteLine(r);
+            }
+            else if(oper == "multiply")
+            {
+                int r = 1;
+                for(i=0;i< n;i++)
+                {
+                    Console.WriteLine("Enter the number " + (i + 1));
+                    inputnum = Int32.Parse(Console.ReadLine());
+                   
+                    r = r * inputnum;
+
+                }
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
