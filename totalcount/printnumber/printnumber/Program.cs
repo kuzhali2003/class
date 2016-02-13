a10fcfd489661bd2d0bd9c04ee197f74844a2f96
kuzhali <kuzhalis@gmail.com>
2016-02-12 21:15:53 -0600

    did it

--- totalcount/printnumber/printnumber/Program.cs
+++ totalcount/printnumber/printnumber/Program.cs
@@ -15,7 +15,7 @@ namespace printnumber
             int i = 1;
             while(i<=n)
             {
-                Console.Write(i+ " " );
+                Console.Write(i+","+ " " );
                 i = i + 1;
 
             }
