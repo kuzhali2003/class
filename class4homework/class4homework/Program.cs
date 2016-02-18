b15ed4d7957aed2c26861de3793152e5dc88753c
kuzhali <kuzhalis@gmail.com>
2016-02-17 22:45:13 -0600

    class4homework

---
+++ class4homework/class4homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class4homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name ?  ");
+            string samplename = Console.ReadLine();
+            Console.WriteLine("string length : " + samplename.Length);
+            Console.WriteLine(samplename.Substring(11));
+            var samplestring = samplename.Substring(11, 19);
+            Console.WriteLine("Hi " + samplestring + ". ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ class4homework/program1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the array : ");
+            var inputsize = Int32.Parse(Console.ReadLine());
+
+            int[] numbarray = new int[inputsize];
+            int i = 0;
+            while (i < numbarray.Length) 
+            { 
+                Console.Write("enter the value at index " + 0 + ":");
+                numbarray[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Array.Sort(numbarray);
+            for ( i = 0; i < numbarray.Length; i++)
+            {
+                Console.WriteLine(numbarray[i]);
+            }
+           
+                Console.WriteLine("Reverse sorted array : ");
+            for (i = numbarray.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(numbarray[i]);
+                
+            }
+
+        }
+
+    }
+}
