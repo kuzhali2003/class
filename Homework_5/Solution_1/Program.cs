69d521ff6d2177e7e94b023f3643ad977dac4c87
kuzhali <kuzhalis@gmail.com>
2016-02-26 10:38:51 -0600

    circle homework5

---
+++ Homework_5/Solution_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    class Circle
+
+    {
+
+
+        public Circle()
+
+        {
+            Console.Write("Please enter the radius: ");
+            int radius = Int32.Parse(Console.ReadLine());
+            int radr1 = radius;
+
+        }
+           public int radr1 { get; set; } = 5;
+
+            public  void  FindArea()
+        {
+            double area = 3.14 * (radr1 * radr1);
+
+            Console.WriteLine("Area of the circle = " + area);
+        
+            
+        }
+    }
+
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an option circle/square/rect/triangle/quit : ");
+            string option = Console.ReadLine();
+            Circle objcircle1 = new Circle();
+            objcircle1.FindArea();
+
+
+        }
+    }
+}
