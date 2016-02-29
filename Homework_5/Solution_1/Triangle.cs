04653095df093a709ca6f666014a1f32c6e8fc33
kuzhali <kuzhalis@gmail.com>
2016-02-29 17:20:51 -0600

    triangle

---
+++ Homework_5/Solution_1/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    class Triangle
+    {
+
+
+        public Triangle()
+
+        {
+            Console.Write("Please enter the base: ");
+            baseb = Int32.Parse(Console.ReadLine());
+            Console.Write("Please enter the height: ");
+            height = Int32.Parse(Console.ReadLine());
+        }
+
+        public int baseb { get; set; }
+        public int height { get; set; }
+
+        public void FindAreaT()
+        {
+
+            double areaT= .5 * baseb * height;
+            Console.WriteLine("Area of the circle = " + areaT);
+        }
+
+
+    }
+}
+
+
+    
+
+
