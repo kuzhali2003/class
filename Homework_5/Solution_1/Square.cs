02b4266bdd4cf64ba417786a7de5f69c8bbe3ece
kuzhali <kuzhalis@gmail.com>
2016-02-29 17:20:22 -0600

    square

---
+++ Homework_5/Solution_1/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    class Square
+    {
+        public Square()
+        {
+            Console.Write("Please enter the side: ");
+            sides = Int32.Parse(Console.ReadLine());
+        }
+        public int sides { get; set; }
+
+        public void FindAreaS()
+        {
+            double areaa = sides * sides;
+            Console.WriteLine("Area of the square = " + areaa);
+        }
+
+    }
+}
