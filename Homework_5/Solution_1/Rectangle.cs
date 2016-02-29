1bfe5657e041e658d395217ddefdd3adc165591d
kuzhali <kuzhalis@gmail.com>
2016-02-29 17:19:16 -0600

    rectangle

---
+++ Homework_5/Solution_1/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    class Rectangle
+    {
+        public Rectangle()
+        {
+            Console.Write("Please Enter the length: ");
+            length = Int32.Parse(Console.ReadLine());
+            Console.Write("Please enter the width: ");
+            width = Int32.Parse(Console.ReadLine());
+
+        }
+        public int length { set; get; }
+        public int width { get; set; }
+        public void FindAreaR()
+        {
+            double arear = length * width;
+            Console.WriteLine("Area of the rectangle: " + arear);
+        }
+    }
+}
