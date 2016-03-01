bd1102f58b6a55825a0f5102acb630355886a2f6
kuzhali <kuzhalis@gmail.com>
2016-03-01 15:20:00 -0600

    corrected

--- Homework_5/Solution_1/Circle.cs
+++ Homework_5/Solution_1/Circle.cs
@@ -7,30 +7,19 @@ using System.Threading.Tasks;
 namespace Solution_1
 {
     class Circle
-
     {
-       
+        private double radius;
+        public Circle()
+        {
+            Console.Write("Please enter the radius: ");
+            radius = UserInput.AcceptNumberInput();
+        }
+        public void FindArea()
+        {
 
-        
-
-
-            public Circle()
-
-            {
-                Console.Write("Please enter the radius: ");
-                 radius = Int32.Parse(Console.ReadLine());
-            }
-
-            public int radius { get; set; }
-
-            public void FindArea()
-            {
-
-                double area = 3.14 * (radius * radius);
-                Console.WriteLine("Area of the circle = " + area);
-            }
-
-
-        
+            double area = Math.PI * (radius * radius);
+            Console.WriteLine("Area of the circle = " + area);
+        }
+     
     }
 }
